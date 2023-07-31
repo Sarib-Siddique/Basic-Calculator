f0f5ad541a7cd40ad2a7f59c462d78321cc49ec1
Sarib Siddiqui <110452117+Sarib-Siddique@users.noreply.github.com>
2023-07-31 08:38:35 +0500

    Add files via upload

---
+++ tasks/script.js
@@ -0,0 +1,68 @@
+var num1 = +prompt("Enter Value 1");
+var num2 = +prompt("Enter Value 1");
+var add = num1 + num2;
+
+document.write(
+  "<table border='1px'>" +
+    "<tr>" +
+    "<th>" +
+    "ADDITION" +
+    "</th>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>" +
+    add +
+    "</td>" +
+    "</tr>" +
+    "</table>"
+);
+
+var sub = num1 - num2;
+
+document.write(
+  "<table border='1px'>" +
+    "<tr>" +
+    "<th>" +
+    "SUBTRACTION" +
+    "</th>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>" +
+    sub +
+    "</td>" +
+    "</tr>" +
+    "</table>"
+);
+
+var multipication = num1 * num2;
+
+document.write(
+  "<table border='1px'>" +
+    "<tr>" +
+    "<th>" +
+    "MULTIPICATION" +
+    "</th>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>" +
+    multipication +
+    "</td>" +
+    "</tr>" +
+    "</table>"
+);
+var division = num1 / num2;
+
+document.write(
+  "<table border='1px'>" +
+    "<tr>" +
+    "<th>" +
+    "DIVISION" +
+    "</th>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>" +
+    division +
+    "</td>" +
+    "</tr>" +
+    "</table>"
+);
